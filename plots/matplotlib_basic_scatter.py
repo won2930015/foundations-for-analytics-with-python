490b26ceaf190c3f8bc6a329541f59a1157ca4c4
won2930015 <58769598@qq.com>
2017-10-08 02:26:44 +0800

    me up1

--- plots/matplotlib_basic_scatter.py
+++ plots/matplotlib_basic_scatter.py
@@ -4,16 +4,15 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
 x = np.arange(start=1., stop=15., step=1.)
-y_linear = x + 5. * np.random.randn(14.)
-y_quadratic = x**2 + 10. * np.random.randn(14.)
+y_linear = x + 5. * np.random.randn(14)	#randn(14)：不接受浮点数.
+y_quadratic = x**2 + 10. * np.random.randn(14)
 
 fn_linear = np.poly1d(np.polyfit(x, y_linear, deg=1))
 fn_quadratic = np.poly1d(np.polyfit(x, y_quadratic, deg=2))
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
-ax1.plot(x, y_linear, 'bo', x, y_quadratic, 'go', \
-			x, fn_linear(x), 'b-', x, fn_quadratic(x), 'g-', linewidth=2.)
+ax1.plot(x, y_linear, 'bo', x, y_quadratic, 'go', x, fn_linear(x), 'b-', x, fn_quadratic(x), 'g-', linewidth=2.)
 ax1.xaxis.set_ticks_position('bottom')
 ax1.yaxis.set_ticks_position('left')
 
